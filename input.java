46927af2417ea7a95fde188224ef54dd105667a1
Shanto <shantonath150@gmail.com>
2024-02-03 10:15:42 +0600

    Add java file

---
+++ input.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class input {
+  public static void main(String args[]){
+    Scanner scn = new Scanner(System.in);
+    // String input = scn.next();
+    // System.out.println(input);
+    // String name = scn.nextLine();
+    // System.out.println(name);
+
+    int  number = scn.nextInt();
+    System.out.println(number);
+
+    float price = scn.nextFloat();
+    System.out.println(price);
+  }
+  
+}
